55eb8e1706b1f57ff5c2c790f69129ae50eae5bd
hanishkvc <hanishkvc@gmail.com>
2022-08-21 11:16:14 +0530

    Add the basic logger trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//!
+//! Help log messages in programs
+//! * into different categories
+//! * inturn logging onto console or file
+//!
+//! HanishKVC, 2022
+//!
+
+trait Logger {
+    fn log_normal(&self, msg: &str);
+    fn log_error(&self, msg: &str);
+    fn log_debug(&self, msg: &str);
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
